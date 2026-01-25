dd3a6e2ea7dfafc462a2ff9c9e06863490fb60de
Zane Shaw <zane@squidee.dev>
2026-01-25 17:22:55 +1100

    add zod validator

--- src/api/braidoku.ts
+++ src/api/braidoku.ts
@@ -1,8 +1,11 @@
 import { levelsDb } from "../data";
 import { generate } from "../games/braidoku";
 import type { Category } from "../types";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { DateTime } from "luxon";
+import z from "zod";
+import { CanonicalTimezoneSchema } from "zod-timezone-validation";
 
 type GenerateReturn = { columns: Category[]; rows: Category[]; grid: number[][][] };
 
@@ -11,42 +14,50 @@ const board: { [key: string]: GenerateReturn } = {};
 
 const app = new Hono();
 
-app.get("/board", async (c) => {
-	if (!c.req.query("tz")) return c.text("missing tz (IANA time zone).", 400);
+app.get(
+	"/board",
+	zValidator(
+		"query",
+		z.object({
+			tz: CanonicalTimezoneSchema,
+		})
+	),
+	async (c) => {
+		const date = DateTime.now().setZone(c.req.valid("query").tz).toISODate() as string;
 
-	const date = DateTime.now().setZone(c.req.query("tz")).toISODate();
+		if (!board[date]) {
+			board[date] = generate(date, 2, 5, 4);
+		}
 
-	if (date == undefined) return c.text("date could not be set.", 500);
-
-	if (!board[date]) {
-		board[date] = generate(date, 2, 5, 4);
+		return c.json({ columns: board[date].columns, rows: board[date].rows });
 	}
+);
 
-	return c.json({ columns: board[date].columns, rows: board[date].rows });
-});
+app.get(
+	"/guess",
+	zValidator(
+		"query",
+		z.object({
+			tz: CanonicalTimezoneSchema,
+			index: z.coerce.number().min(0).max(8),
+			world: z.coerce.number(),
+			level: z.coerce.number(),
+		})
+	),
+	async (c) => {
+		const date = DateTime.now().setZone(c.req.valid("query").tz).toISODate() as string;
 
-app.get("/guess", async (c) => {
-	if (!c.req.query("tz")) return c.text("missing tz (IANA time zone).", 400);
-
-	const date = DateTime.now().setZone(c.req.query("tz")).toISODate();
-
-	if (date == undefined) return c.text("date could not be set.", 500);
-
-	if (!board[date]) {
-		board[date] = generate(date, 2, 5, 4);
-	}
-
-	if (c.req.query("index") && c.req.query("world") && c.req.query("level")) {
-		const [cellIndex, world, level] = [c.req.query("index"), c.req.query("world"), c.req.query("level")].map((x) => parseInt(x!));
+		if (!board[date]) {
+			board[date] = generate(date, 2, 5, 4);
+		}
 
 		const levels = Object.values(levelsDb.flat);
 
-		const correct = board[date!]!.grid.flat()[cellIndex!]!.some((levelIndex) => levels[levelIndex]!.world == world && levels[levelIndex]!.level == level);
+		const cell = board[date].grid.flat()[c.req.valid("query").index]!;
+		const correct = cell.some((levelIndex) => levels[levelIndex]!.world == c.req.valid("query").world && levels[levelIndex]!.level == c.req.valid("query").level);
 
-		return c.json({ correct });
-	} else {
-		return c.text("missing index, world, or level.", 400);
+		return c.json(correct);
 	}
-});
+);
 
 export default app;
