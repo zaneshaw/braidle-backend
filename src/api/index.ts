d226fd28e1a751e8f01135b18cbef4bb4479954f
Zane Shaw <zane@squidee.dev>
2026-01-26 09:04:43 +1100

    add cors middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,11 @@ import braidoku from "./braidoku";
 import levels from "./levels";
 import puzzlePiece from "./puzzle_piece";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
 const app = new Hono();
+app.use("*", cors());
 app.use(logger());
 
 app.route("/levels", levels);
